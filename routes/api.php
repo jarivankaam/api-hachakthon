4efb22e132e360f6902606a547360c539f4237fc
jarivankaam <jari@redbanana.nl>
2023-01-26 09:20:07 +0100

    api-hackaton-backend | added db migration for image

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
-});
 
 
 
