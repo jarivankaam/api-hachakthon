4b0f62563a8bd08ab059ff93f6d00f818da3676b
BoazWelsenis <D281029@edu.rocwb.nl>
2023-01-26 14:03:20 +0100

    Added DB Insert into uploadImage, so that it stores the name of all the images in the Database

--- app/Http/Controllers/imageController.php
+++ app/Http/Controllers/imageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 use \App\Models\Image;
@@ -16,6 +17,7 @@ class imageController extends Controller
     public function uploadImage(Request $request)
     {
         $path = Storage::putFile('image', $request->image);
+        DB::insert('insert into images (filename) values (?)', array( $path ));
         return response()->json(['path' => $path]);
     }
 
